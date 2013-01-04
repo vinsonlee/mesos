7eccd8488438ce2ffecdcc09ee45f205840f8580
TD Mackey <tdmackey@booleanhaiku.com>
2013-01-03 23:50:15 -0800

    Revert "Added profiler endpoints in libprocess."
    This reverts commit 71b7a93e87e849b3d9d9263f058d1fb31365c529.

--- third_party/libprocess/src/process.cpp
+++ third_party/libprocess/src/process.cpp
@@ -57,7 +57,6 @@
 #include <process/io.hpp>
 #include <process/mime.hpp>
 #include <process/process.hpp>
-#include <process/profiler.hpp>
 #include <process/socket.hpp>
 #include <process/thread.hpp>
 #include <process/timer.hpp>
@@ -1390,9 +1389,6 @@ void initialize(const string& delegate)
 
   // Create global garbage collector.
   gc = spawn(new GarbageCollector());
-
-  // Create the global profiler.
-  spawn(new Profiler(), true);
 
   // Initialize the mime types.
   mime::initialize();
